aabaef4a7832b7963ed3074995b08baa66f29bde
Agam Dua <agam.dua@dealertrack.com>
2015-06-22 22:36:36 -0400

    More skeleton code

--- burrow/burrow.py
+++ burrow/burrow.py
@@ -1,10 +1,27 @@
 # -*- coding: utf-8 -*-
+import requests
 
 # Currently only post is supported. GET requires a way to handle urls
 HTTP_METHODS = ('POST',)
 
 
-def _make_request(requset, response=None):
+def _get_config_attrs(request):
+    """Hack to get config attrs which are class variables
+
+    Cannot think of a situation where a caller should need to access this
+    directly so it is marked private"""
+    return {k: v for k, v in vars(request).items() if not k.startswith('__')}
+
+
+def _sanitize_config_params(request):
+    """this sanitizes the params passed to the requests method
+
+    Q: should this be done after _get_config_attrs or before?
+    """
+    raise NotImplementedError
+
+
+def _make_request(request, response=None):
     """This is the code that actually makes the request.
 
     Passing the response is optional as the top level requests are not expected
@@ -14,6 +31,15 @@ def _make_request(requset, response=None):
 
     Cannot think of a situation where a caller should need to access this
     directly so it is marked private"""
+    f = getattr(requests, request.method)
+
+    if response:
+        raise NotImplementedError
+
+    # TODO: need to sanitize as well before this will actually work
+    # one example is `fail_mode` blows up because requests doesn't know
+    # what to do with it
+    f(**_get_config_attrs(request))
 
 
 def burrower(routes):
@@ -26,9 +52,8 @@ def burrower(routes):
         nested_route = routes[request].get(response.status_code)
 
         if not nested_route:
-            # handle_failure() to raise exception or call fail mode
-            # or `continue`
-            raise NotImplementedError
+            # TODO: handle_failure() to raise exception or call fail mode
+            continue
 
         # limited to one level deep for simplicity during the prototyping phase
         _make_request(nested_route, response)
